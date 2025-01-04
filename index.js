24cb8364752e34eca58eae0194e2d3e8d70e3c6a
Farjana Eti <etimourakkhieti@gmail.com>
2025-01-04 20:03:54 +0600

    user

--- index.js
+++ index.js
@@ -25,11 +25,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+     const userCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("cart");
 
+
+     // users related api
+      app.post('/users', async (req, res) => {
+      const user = req.body;
+       // insert email if user doesnt exists: 
+      // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+      })
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     //get all menu data
      app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
